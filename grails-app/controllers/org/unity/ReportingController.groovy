6106f63b34d848a9475d4cd7d44b914fd8ac8509
Ace Pineda <acebgp04@gmail.com>
2015-06-10 14:15:54 +0800

    - reporting

---
+++ grails-app/controllers/org/unity/ReportingController.groovy
@@ -0,0 +1,17 @@
+package org.unity
+
+import org.springframework.security.access.annotation.Secured
+import pl.touk.excel.export.XlsxExporter
+
+@Secured(['ROLE_ADMIN'])
+class ReportingController {
+
+    def index() {
+        List<Player> players = Player.findAllByTeam(Team.get(5))
+
+        def withProperties = ['firstName', 'lastName', 'team.name', 'subTeam.name', 'sports']
+        new XlsxExporter('D:\\Report.xlsx').
+                add(players, withProperties).
+                save()
+    }
+}
